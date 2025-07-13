64b410a0d456f0378743d1ec7c29de70133a86dc
lebogang esther <122855360+EstherLebogang@users.noreply.github.com>
2025-07-13 02:09:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,8 @@
         result.innerHTML = "";
 
         try {
-          const response = await fetch("http://localhost:4000/api/download/mp3", {
+          const response = await fetch("https://yt-mp3-lebogang.onrender.com/api/download/mp3", {
+
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ url }),
